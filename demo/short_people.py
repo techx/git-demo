81665f40b021ef15a9ba2275602e6ec991999533
Tony Cui <tacui@mit.edu>
2022-09-28 00:53:15 -0400

    short people class, restructure files

---
+++ demo/short_people.py
@@ -0,0 +1,11 @@
+
+
+class ShortPeopleIdentifier:
+    short_people = set(["joyce","hannak","hannaz"])
+
+    def check(name):
+        return True if name in ShortPeopleIdentifier.short_people else False
+
+if __name__ == '__main__':
+    name = 'joyce'
+    print(ShortPeopleIdentifier.check('joyce'))
