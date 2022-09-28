5eb8e16fc608c7a656838e85affc876049f0018f
Tony Cui <tacui@mit.edu>
2022-09-28 00:46:09 -0400

    favorite foods

---
+++ tonys_favorite_foods.py
@@ -0,0 +1,16 @@
+
+
+class Foods:
+    def __init__(self,foods) -> None:
+        self.foods = foods
+
+    def request(self,i):
+        return self.foods[i] if i < len(self.foods) else None
+
+    def __repr__(self) -> str:
+        return "you <3"
+favorites = ['bbb','peaches','strawberries','dimsum']
+
+if __name__ == '__main__':
+    tonys_foods = Foods(favorites)
+    print(tonys_foods.request(0))
